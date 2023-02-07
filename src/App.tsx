787c396c03efaaad7b70950908b39f059deb092e
javi-diaz47 <javiereduardo300@gmail.com>
2023-02-07 12:21:03 -0500

    chore: add eslint and prettier config

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+  return (
+    <div className="App">
+      <h2>Hello, world!</h2>
+    </div>
+  );
+};
