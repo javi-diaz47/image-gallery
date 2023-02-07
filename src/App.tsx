43543bcfdd0af5ec6cfc34315249a642104e6455
javi-diaz47 <javiereduardo300@gmail.com>
2023-02-07 13:02:00 -0500

    chore: add api key and the version of react in eslintrc

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 
 export const App = (): JSX.Element => {
+  console.log(import.meta.env.VITE_PEXEL_API_KEY);
   return (
     <div className="App">
       <h2>Hello, world!</h2>
